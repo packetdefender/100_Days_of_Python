8b4a314d475f7acc497de2aea2a069930c23b173
Mike Lossmann <michaellossmann@gmail.com>
2022-01-30 12:22:56 -0500

    Redo of Day 19

---
+++ day-19/etch-a-sketch.py
@@ -0,0 +1,38 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forward():
+    tim.forward(10)
+
+
+def move_backward():
+    tim.backward(10)
+
+
+def move_left():
+    tim.left(10)
+
+
+def move_right():
+    tim.right(10)
+
+
+def clear_screen():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
+screen.listen()
+
+screen.onkey(move_forward, "w")
+screen.onkey(move_backward, "s")
+screen.onkey(move_right, "d")
+screen.onkey(move_left, "a")
+screen.onkey(clear_screen, "c")
+
+screen.exitonclick()
