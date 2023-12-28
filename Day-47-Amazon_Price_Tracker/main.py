8e7899bbec4190d6dac410f9a5916fe7db405fc8
Mike Lossmann <michaellossmann@gmail.com>
2023-12-27 21:13:32 -0500

    Recommit

--- Day-47-Amazon_Price_Tracker/main.py
+++ Day-47-Amazon_Price_Tracker/main.py
@@ -4,10 +4,10 @@ import smtplib
 import os
 
 URL = "https://www.amazon.com/Anker-PowerCore-Portable-Charger-Compatible/dp/B09VPHVT2Z/ref=sr_1_3?keywords=anker+737+power+bank&qid=1663116501&sprefix=anker+737+%2Caps%2C91&sr=8-3"
-SMTP_USERNAME = os.environ["SMTP_USERNAME"]
-SMTP_PASSWORD = os.environ["SMTP_PASSWORD"]
-SMTP_SERVER = "smtp.xxx.xxx"
-SMTP_PORT = 587
+#SMTP_USERNAME = os.environ["SMTP_USERNAME"]
+#SMTP_PASSWORD = os.environ["SMTP_PASSWORD"]
+#SMTP_SERVER = "smtp.xxx.xxx"
+#SMTP_PORT = 587
 TARGET_PRICE = 100.00
 
 
@@ -27,18 +27,19 @@ if total_amount <= TARGET_PRICE:
     TO = "xxxx@xxxx.com"
     MSG = f"subject: Anker PowerCore is now {total_amount}, buy now! \n\nThe anker powercore 737 you are a looking at" \
           f" is below the threshold amount of ${TARGET_PRICE}!  Click here to buy it now {URL}"
-    mail = smtplib.SMTP(host=SMTP_SERVER, port=SMTP_PORT)
-    mail.starttls()
-    mail.login(SMTP_USERNAME, SMTP_PASSWORD)
-    mail.sendmail(FROM, TO, MSG)
+ #   mail = smtplib.SMTP(host=SMTP_SERVER, port=SMTP_PORT)
+ #   mail.starttls()
+ #   mail.login(SMTP_USERNAME, SMTP_PASSWORD)
+ #   mail.sendmail(FROM, TO, MSG)
+    print(MSG)
 else:
     FROM = "xxxx@xxxx.com"
     TO = "xxxx@xxxx.com"
     MSG = f"subject: Anker PowerCore price is {total_amount}. \n\nThe price of the anker powercore 737 you are a looking " \
           f"at is ${TARGET_PRICE}.  No need to purchase yet"
-    mail = smtplib.SMTP(host=SMTP_SERVER, port=SMTP_PORT)
-    mail.starttls()
-    mail.login(SMTP_USERNAME, SMTP_PASSWORD)
-    mail.sendmail(FROM, TO, MSG)
-
+  #  mail = smtplib.SMTP(host=SMTP_SERVER, port=SMTP_PORT)
+  #  mail.starttls()
+  #  mail.login(SMTP_USERNAME, SMTP_PASSWORD)
+  #  mail.sendmail(FROM, TO, MSG)
+    print(MSG)
 
